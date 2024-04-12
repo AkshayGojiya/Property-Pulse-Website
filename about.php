7fdfc806482c44bbbe6a948145b665b3622d96e2
Akshay <aksgojiya@gmail.com>
2024-04-12 19:24:10 +0530

    changes

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Property Pulse | Let Us Guide You Home</title>
     <link rel="icon" type="image/x-icon" href="Images/android-chrome-512x512.png">
-    <link rel="stylesheet" type="text/css" href="CSS/about.css">
+    <link rel="stylesheet" type="text/css" href="about.css">
 </head>
 <body>
     <header>
--- agent.php
+++ agent.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Property Pulse | Let Us Guide You Home</title>
-    <link rel="stylesheet" type="text/css" href="CSS/agent.css">
+    <link rel="stylesheet" type="text/css" href="agent.css">
     <link rel="icon" type="image/x-icon" href="Images/android-chrome-512x512.png">
 </head>
 <body>
@@ -12,7 +12,37 @@
         <?php require("header.php"); ?>
     </header>
     <section>
-        <div class="ag"></div>
+        <div class="agent">
+            <div class="agentmain">
+                    <div class="agentupper">
+                        Agents
+                    </div>
+                    <div class="agentlineone"></div>
+                    <div class="agentlinetwo"></div>
+                    <div class="agentlover">
+                        <div class="agentloverin">
+                            <div class="agentloverinimg"><img src="Images/agent.png" alt="Agent"></div>
+                            <div class="agentloverintext1">Agent One</div>
+                            <div class="agentloverintext2">Real Estate - Agent</div>
+                        </div>
+                        <div class="agentloverin">
+                            <div class="agentloverinimg"><img src="Images/agent.png" alt="Agent"></div>
+                            <div class="agentloverintext1">Agent Two</div>
+                            <div class="agentloverintext2">Real Estate - Agent</div>
+                        </div>
+                        <div class="agentloverin">
+                            <div class="agentloverinimg"><img src="Images/agent.png" alt="Agent"></div>
+                            <div class="agentloverintext1">Agent Three</div>
+                            <div class="agentloverintext2">Real Estate - Agent</div>
+                        </div>
+                        <div class="agentloverin">
+                            <div class="agentloverinimg"><img src="Images/agent.png" alt="Agent"></div>
+                            <div class="agentloverintext1">Agent Four</div>
+                            <div class="agentloverintext2">Real Estate - Agent</div>
+                        </div>
+                    </div>
+            </div>
+        </div>
     </section>
     <footer>
         <?php require("footer.php"); ?>
